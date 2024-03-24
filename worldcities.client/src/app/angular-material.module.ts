e13ae363db45726ce1ca97220303172cc1894cce
Jon Croce <crocedev@gmail.com>
2024-03-23 20:54:48 -0400

    Add client-side pagination to Cities component
    Uses Material Design Paginator, adding it to the Angular Material module.

--- worldcities.client/src/app/angular-material.module.ts
+++ worldcities.client/src/app/angular-material.module.ts
@@ -3,19 +3,22 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ]
 })
 export class AngularMaterialModule { }
--- worldcities.client/src/app/cities/cities.component.ts
+++ worldcities.client/src/app/cities/cities.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { City } from './city';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import type { City } from './city';
 
 @Component({
   selector: 'app-cities',
@@ -12,14 +14,18 @@ export class CitiesComponent implements OnInit {
   public displayedColumns: Array<string> = [
     'id', 'name', 'lat', 'lon'
   ];
-  public cities!: Array<City>;
+  public cities!: MatTableDataSource<City>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     var apiEndpoint = environment.baseUrl + 'api/Cities';
     this.http.get<Array<City>>(apiEndpoint).subscribe({
-      next: (result) => { this.cities = result },
+      next: (result) => {
+        this.cities = new MatTableDataSource<City>(result);
+        this.cities.paginator = this.paginator;
+      },
       error: (error) => console.error(error)
     });
   }
