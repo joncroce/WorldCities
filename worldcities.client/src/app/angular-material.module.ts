b0dfef24fc1dc7de4a4de0c3cc7d9ea724a1b303
Jon Croce <crocedev@gmail.com>
2024-03-23 22:11:26 -0400

    Implement server-side pagination/sorting/filtering of cities
    Adds ApiResult class with generic type.
    Adds NuGet package System.Linq.Dynamic.Core.
    Adds Angular Material Sort and Input modules.

--- worldcities.client/src/app/angular-material.module.ts
+++ worldcities.client/src/app/angular-material.module.ts
@@ -4,6 +4,8 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   imports: [
@@ -11,14 +13,18 @@ import { MatPaginatorModule } from "@angular/material/paginator";
     MatIconModule,
     MatToolbarModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule
   ]
 })
 export class AngularMaterialModule { }
