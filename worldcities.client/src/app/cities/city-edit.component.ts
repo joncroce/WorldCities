4ad295d89a48a5079eca5410c9d4c5cb7018d9e5
Jon Croce <crocedev@gmail.com>
2024-03-24 14:42:26 -0400

    Re-order import statements

--- worldcities.client/src/app/cities/city-edit.component.ts
+++ worldcities.client/src/app/cities/city-edit.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AbstractControl, AsyncValidatorFn, FormControl, FormGroup, Validators } from '@angular/forms';
-import { environment } from '../../environments/environment';
-import { City } from './city';
-import { Country } from './../countries/country';
 import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Country } from './../countries/country';
+import { City } from './city';
 
 @Component({
   selector: 'app-city-edit',
