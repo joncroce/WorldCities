b254e47158bb26f426d65a36a07251390f5f970b
Jon Croce <crocedev@gmail.com>
2024-03-23 20:04:23 -0400

    Refactor component configurations
    Changes styleUrl to styleUrls in Component decorator configuration objects.
    Removes unused WeatherForecast type.

--- worldcities.client/src/app/app.component.ts
+++ worldcities.client/src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'WorldCities';
