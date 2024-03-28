be135a79430bda8491f79f5db7246d71c5cf1a68
Jon Croce <crocedev@gmail.com>
2024-03-27 21:58:42 -0400

    Setup backend testing
    Adds xUnit Test project with Moq.
    Adds a simple test of the CitiesController's GetCity method using an in-memory database.

---
+++ WorldCities.Server.Tests/CitiesController_Test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Controllers;
+using WorldCities.Server.Data;
+using WorldCities.Server.Data.Models;
+
+namespace WorldCities.Server.Tests;
+public class CitiesController_Test
+{
+    /// <summary>
+    /// Test the GetCity() method
+    /// </summary>
+    [Fact]
+    public async Task GetCity()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "WorldCities")
+            .Options;
+        
+        using var context = new ApplicationDbContext(options);
+        context.Add(new City()
+        {
+            Id = 1,
+            CountryId = 1,
+            Lat = 1,
+            Lon = 1,
+            Name = "TestCity1"
+        });
+        context.SaveChanges();
+
+        var controller = new CitiesController(context);
+        City? city_existing = null;
+        City? city_notExisting = null;
+
+        // Act
+        city_existing = (await controller.GetCity(1)).Value;
+        city_notExisting = (await controller.GetCity(2)).Value;
+
+        // Assert
+        Assert.NotNull(city_existing);
+        Assert.Null(city_notExisting);
+    }
+}
